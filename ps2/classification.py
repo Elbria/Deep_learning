af9560f31dd402fb95918bc5c5ad8c4872f28478
Elbria <elmpriakou@gmail.com>
2018-09-28 16:31:03 -0400

    baseline perception words/mnist

--- ps2/classification.py
+++ ps2/classification.py
@@ -1,2 +1,42 @@
+from __future__ import print_function
 import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+
+batch_size = 128
+num_classes = 10
+epochs = 1
+
+x_train = np.load('MNIST/trainImages.npy')
+y_train = np.load('MNIST/trainLabels.npy')
+x_test = np.load('MNIST/testImages.npy')
+y_test = np.load('MNIST/testLabels.npy')
+
+num_pixels = x_train.shape[2] * x_train.shape[3]
+x_train = x_train.reshape(x_train.shape[0], num_pixels).astype('float32')
+x_test = x_test.reshape(x_test.shape[0], num_pixels).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+x_train = x_train / 255
+x_test = x_test / 255
+
+# one hot encode outputs
+num_classes = y_test.shape[1]
+
+# define baseline model
+model = Sequential()
+model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit model
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
